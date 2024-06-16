c671a5ceee4e35f376f18d768a7067ab8f453934
fs-kunsaid <fs.kunsaid@mail.ru>
2024-06-16 16:15:09 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -2,6 +2,7 @@ immutable_var = 1, 2, 3, 4, 5, 'слово', True, 2.5
 print(immutable_var)
 print(type(immutable_var))
 # immutable_var[3] = 10
+# Кортеж является неизменяемым объектом. В кортеже нельзя изменить или добавить элементы. 
 
 mutable_list = [1, 2, 3, 4, 5, 'слово', True, 2.5]
 print(mutable_list)
