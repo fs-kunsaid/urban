ed1f782ae54156d36eb942019da397f134d673f4
fs-kunsaid <fs.kunsaid@mail.ru>
2024-06-17 16:27:11 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,20 @@
+# Словарь
+my_dict = {'Max': 1987, 'Fedor': 1978, 'Ben': 1956}
+print(my_dict)
+print(my_dict['Max'])
+print(my_dict.get('Elena', 'Такого значения нет'))
+my_dict.update({'Elena': 1945, 'Ivan': 1990})
+print(my_dict)
+del my_dict['Ben']
+print(my_dict)
+# Множество
+my_set = {5, 8, 13, 21, True, 8, 21, False, 13, 34}
+print(my_set)
+my_set.update({55, 89})
+my_set.add(144)
+print(my_set)
+my_set.discard(3)
+my_set.remove(34)
+print(my_set)
+
+
