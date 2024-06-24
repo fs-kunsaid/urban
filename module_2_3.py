77a0f5237a25fd361ab87809219c8c5991d96c9a
fs-kunsaid <fs.kunsaid@mail.ru>
2024-06-24 19:36:28 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,15 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-
 number = 0
 
 while number < len(my_list):
-    number2 = my_list[number]
-    if number2 < 0:
+    current_number = my_list[number]
+
+    if current_number < 0:
         break
-    print(number2)
+
+    if current_number == 0:
+        number += 1
+        continue
+
+    print(current_number)
     number = number + 1
