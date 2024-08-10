d6bf6bb1d2084feafb3ad353d9d50f0a134d0310
fs-kunsaid <fs.kunsaid@mail.ru>
2024-08-10 14:49:45 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as dfk_m
+from true_math import divide as dtr_m
+
+result1 = dfk_m(69, 3)
+result2 = dfk_m(3, 0)
+result3 = dtr_m(49, 7)
+result4 = dtr_m(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
