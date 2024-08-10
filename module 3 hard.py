39f4b51b27fddbcdf4bf18b99dfe6f2f439cb24e
fs-kunsaid <fs.kunsaid@mail.ru>
2024-08-10 13:28:13 +0300

    Add files via upload

---
+++ module 3 hard.py
@@ -0,0 +1,25 @@
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+def calculate_structure_sum(data_structure):
+    sum = 0
+    if isinstance(data_structure, dict):
+        for i, j in data_structure.items():
+            sum += calculate_structure_sum(i)
+            sum += calculate_structure_sum(j)
+    elif isinstance(data_structure, (list, tuple, set)):
+        for k in data_structure:
+            sum += calculate_structure_sum(k)
+    elif isinstance(data_structure, (int, float)):
+        sum += data_structure
+    elif isinstance(data_structure, str):
+        sum += len(data_structure)
+    return sum
+
+result = calculate_structure_sum(data_structure)
+print(result)
